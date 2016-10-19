02a0d64db8678f36671cf8b3ff6c2c9517f79777
Macphil11 <philippe.mondon@gmail.com>
2016-10-19 22:23:08 +0200

    stable maven configuration

--- XML-KAFKA-PRODUCER/tests/org/thegreenseek/samples/kafka/xml/XMLKafkaProducer$Test.scala
+++ XML-KAFKA-PRODUCER/tests/org/thegreenseek/samples/kafka/xml/XMLKafkaProducer$Test.scala
@@ -39,7 +39,7 @@ class XMLKafkaProducer$Test extends FunSuite with BeforeAndAfterEach {
     }
 
     XMLKafkaProducer.parseXmlFile(
-      "/Users/Macphil1/Documents/Projets/GitHub/XML-PROCESSING/XML-KAFKA-PRODUCER/data/landsat-medium.b.xml", printMessage, null, buf, topic
+      "/Users/Macphil11/Documents/Projets/GitHub/XML-PROCESSING/XML-KAFKA-PRODUCER/data/landsat-medium.b.xml", printMessage, null, buf, topic
     )
 
   }
