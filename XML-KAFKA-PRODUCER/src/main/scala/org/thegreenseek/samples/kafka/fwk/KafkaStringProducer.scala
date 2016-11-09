a49cc4e16abb69c3cd9ec84c270fe960009dc997
macphil1 <philippe.mondon@gmail.com>
2016-11-09 23:34:32 +0100

    KafkaStringProducer first version

--- XML-KAFKA-PRODUCER/src/main/scala/org/thegreenseek/samples/kafka/fwk/KafkaStringProducer.scala
+++ XML-KAFKA-PRODUCER/src/main/scala/org/thegreenseek/samples/kafka/fwk/KafkaStringProducer.scala
@@ -7,25 +7,34 @@ import org.apache.kafka.clients.producer._
 /**
   * Created by Macphil1 on 29/10/2016.
   */
-class KafkaStringProducer (properties: Properties) {
-  var dproducer = new KafkaProducer[String, String](properties)
+class KafkaStringProducer {
+  //var dproducer = new KafkaProducer[String, String](properties)
+  var dProperties = KafkaUtilities.loadDefaultProperties
 
   /**
     * this message sender function will be used by file parsers
     * to feed kafka topics
     *
-    * @param lproducer
-    * @param message
+    * @param specProperties
     * @param topic
+    * @param key
+    * @param message
     * @return
     */
-  def sendRecord(lproducer: Any, message: String, topic: String): Int = {
+  def sendRecord(specProperties: Any, topic: String, key: String , message: String): Int = {
 
-    /*Option(lproducer) match {
-      case None => lproducer = dproducer
-    }
-    producer = */
+    var lProperties = new Properties()
+
+    if(!Some(specProperties).isDefined) //If producer with special properties does not exist
+      lProperties = dProperties.head //Use default producer
+
+    if(Some(topic).isDefined && Some(key).isDefined && Some(message).isDefined ) {
+      val record = new ProducerRecord(topic, key, message)
+      val producer: KafkaProducer[String, String] = new KafkaProducer[String, String](lProperties)
+      producer.send(record)
 
       return 0
+    }
+    return -1
   }
 }
